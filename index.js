ea2725b369afbbd104bdf356faa881a6014a0557
harshj-1703 <harshj6680@gmail.com>
2022-10-01 12:58:06 +0530

    nodejs assignment 2

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
-const hello = () => {
-    console.log("Hello Node.js!");
-}
+const http = require('http');
+const fs = require('fs');
+const arguments = require('minimist')(process.argv.slice(2));   //get 2 argument in from commandline one is key and another is value
 
-hello();
+let homefile ='';
+let projectfile = '';
+let registrationfile = '';
+
+fs.readFile("./home.html", (err, home) => {         //read home.html file in homefile variable
+    if(err)
+    {
+        throw err;
+    }
+    homefile = home;
+});
+
+fs.readFile("./registration.html", (err, registration) => {         //read registration.html file in registrationfile variable
+    if(err)
+    {
+        throw err;
+    }
+    registrationfile = registration;
+});
+
+fs.readFile("./project.html", (err, project) => {         //read project.html file in projectfile variable
+    if(err)
+    {
+        throw err;
+    }
+    projectfile = project;
+});
+
+fs.readFile("./home.html",(err, home) => {
+    if(err)
+    {
+        throw err;
+    }
+    http.createServer((req,res) => {
+        let url = request.url;
+        res.writeHeader(200, {"content-type" : "text/html"});
+        if(url == "/project")
+        {
+            res.write(projectfile);
+            res.end;
+        }
+        else if(url == "/registration")
+        {
+            res.write(registrationfile);
+            res.end;
+        }
+        else
+        {
+            res.write(homefile);
+            res.end;
+        }
+    }).listen(args.port);
+});
